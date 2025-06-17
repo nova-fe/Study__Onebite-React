84e2bbde23b39f25e9215da429d170b1ae1d71fa
Nova <heetask@gmail.com>
2025-06-17 14:17:28 +0900

    Update - 투두 수정하기

--- section08/src/components/TodoItem.jsx
+++ section08/src/components/TodoItem.jsx
@@ -1,9 +1,13 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isdone, content, date }) => {
+const TodoItem = ({ id, isdone, content, date, onUpdate }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
   return (
     <div className="TodoItem">
-      <input checked={isdone} type="checkbox" />
+      <input readOnly onChange={onChangeCheckbox} checked={isdone} type="checkbox" />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
