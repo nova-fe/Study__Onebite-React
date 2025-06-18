97d30886f9be37b51423d5304661c7098ba356d3
Nova <heetask@gmail.com>
2025-06-18 17:30:01 +0900

    New 페이지 구현하기 1. UI

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,4 +1,13 @@
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import Header from "../components/Header";
+
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로 가기"} />} />
+      <Editor />
+    </div>
+  );
 };
 export default New;
