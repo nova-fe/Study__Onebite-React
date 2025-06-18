72afbfebdd01790ca36675f3330631ee1ff036f7
Nova <heetask@gmail.com>
2025-06-18 11:28:01 +0900

    공통 컴포넌트 구현하기

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -3,8 +3,7 @@ import { useSearchParams } from "react-router-dom";
 const Home = () => {
   const [params, setParams] = useSearchParams();
   // /?value=hello 일 경우
-  console.log(params.get("value")); // hello
-
+  // console.log(params.get("value")); // hello
   return <div>Home</div>;
 };
 export default Home;
