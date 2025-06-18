898d2566ba2c23f9d24b47f8e9fa69847dd9a7af
Nova <heetask@gmail.com>
2025-06-18 10:55:24 +0900

    페이지 라우팅 4. 동적 경로

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-  return <div>Diary</div>;
+  const params = useParams(); // url 파라미터가 들어옴
+  console.log(params);
+
+  return <div>{params.id}번 일기입니다.</div>;
 };
 export default Diary;
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,4 +1,10 @@
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+  const [params, setParams] = useSearchParams();
+  // /?value=hello 일 경우
+  console.log(params.get("value")); // hello
+
   return <div>Home</div>;
 };
 export default Home;
