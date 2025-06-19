8bbd9f3f8abb08b26a605d5a3262290e5119c275
Nova <heetask@gmail.com>
2025-06-19 10:03:47 +0900

    New 페이지 구현하기 2. 기능

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,12 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
+  const nav = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const onSubmit = (input) => {
+    // onCreate(createdDate, emotionId, content)
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true }); //  replace: true -> 뒤로가기 방지
+  };
+
   return (
     <div>
-      <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로 가기"} />} />
-      <Editor />
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+      />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
